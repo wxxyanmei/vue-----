b56b79edc7997e455555cd62e239a3734e509165
wxxyanmei <736692897@qq.com>
2022-10-22 16:47:08 +0800

    嵌套副作用函数

--- proxy3.0.js
+++ proxy3.0.js
@@ -2,7 +2,7 @@
  * @Author: wxxyanmei 736692897@qq.com
  * @Date: 2022-10-21 23:01:10
  * @LastEditors: wxxyanmei 736692897@qq.com
- * @LastEditTime: 2022-10-22 15:57:36
+ * @LastEditTime: 2022-10-22 16:41:14
  * @FilePath: \vue设计与实现\proxy.3.0.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
---
+++ proxy4.0.js
@@ -0,0 +1,74 @@
+/*
+ * @Author: wxxyanmei 736692897@qq.com
+ * @Date: 2022-10-22 16:04:08
+ * @LastEditors: wxxyanmei 736692897@qq.com
+ * @LastEditTime: 2022-10-22 16:41:15
+ * @FilePath: \vue设计与实现\proxy4.0.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 用一个全局变量存储当前激活的effect函数
+let activeEffect
+const bucket = new WeakMap()
+const data = {
+    ok: true,
+    text: 'hello word'
+}
+
+const effectStack = [] //effect栈
+
+function effect(fn) {
+    const effectFn = () => {
+        activeEffect = effectFn
+        fn()
+    }
+    //activeEffect.deps[]来存储所有与改副作用函数相关的依赖集合
+    effectFn.deps = []
+    //执行副作用函数
+    effectFn()
+}
+
+const obj = new Proxy(data, {
+    get(target, key) {
+        //收集依赖
+        track(target, key)
+        return target[key]
+    },
+    set(target, key, newValue) {
+        //触发副作用函数
+        target[key] = newValue
+        trigger(target, key)
+        
+    }
+})
+
+//清空当前effect函数的依赖
+const cleanUp = (effectFn) => {
+    for (let i = 0; i < effectFn.deps; i++) {
+        const deps = effectFn[i]
+        deps.delete(effectFn)
+    }
+}
+
+//收集依赖的函数
+const track = (target, key) => {
+    if (!activeEffect) return track[key]
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()))
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()))
+    }
+    deps.add(activeEffect)
+    activeEffect.deps.push(deps)
+}
+
+//触发更新的函数
+const trigger = (target, key) =>{
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+    const effects = depsMap.get(key)
+    const effectToRun = new Set(effects)
+    effectToRun.forEach(effects => effects())
+}
